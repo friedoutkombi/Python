66ef5e5b1cc4aaa921ce224b3862303a10f6e01d
friedoutkombi <nicholastdyer@gmail.com>
2017-05-14 23:16:08 +0800

    Update OddorEven.py

--- OddorEven.py
+++ OddorEven.py
@@ -43,3 +43,29 @@ if num%check==0:
     print(str(check)+" divides evenly into "+str(num))
 else:
     print(str(check)+" does not divide evenly into "+str(num))
+
+    
+  #THEIR SOLUTION
+num = input("Enter a number: ")
+mod = num % 2
+if mod > 0:
+    print("You picked an odd number.")
+else:
+    print("You picked an even number.")
+    
+ ##THEIR SOLUTION W/EXTRA
+num = int(input("give me a number to check: "))
+check = int(input("give me a number to divide by: "))
+
+if num % 4 == 0:
+    print(num, "is a multiple of 4")
+elif num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
+
+if num % check == 0:
+    print(num, "divides evenly by", check)
+else:
+    print(num, "does not divide evenly by", check)
+    
